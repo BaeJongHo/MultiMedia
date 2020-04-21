4b4aaa30ec63a34f2d51cacdbc3bcf24af491f2d
BaeJongHo <44634707+BaeJongHo@users.noreply.github.com>
2020-04-21 14:02:28 +0900

    Image printing

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+int main(int argc, char* argv[])
+{
+	Mat img(256, 256, CV_8U, 255);
+	namedWindow("Mat img", WINDOW_AUTOSIZE);
+	imshow("Mat img", img);
+	waitKey(0);
+	return 0;
+}
